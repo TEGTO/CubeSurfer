b7e1d3e12876c7a16efe0656879ecc8a592e3827
TEGTO <90476119+TEGTO@users.noreply.github.com>
2024-02-05 17:34:41 +0200

    commit6
    bug fix

--- Assets/!/Code/Scripts/Player/InputManager.cs
+++ Assets/!/Code/Scripts/Player/InputManager.cs
@@ -4,35 +4,32 @@ using UnityEngine.InputSystem.Controls;
 
 public class InputManager : MonoBehaviour
 {
-    private TouchInput touchInput;
+	private static TouchInput touchInput;
 
-    public delegate void TouchingEvent(TouchControl touch);
-    public event TouchingEvent OnTouchPerfoming;
-    public event TouchingEvent OnTouchCanceled;
+	public delegate void TouchingEvent(TouchControl touch);
+	public event TouchingEvent OnTouchPerfoming;
+	public event TouchingEvent OnTouchCanceled;
 
-    private void Awake()
-    {
-        touchInput = new TouchInput();
-    }
-    private void OnEnable()
-    {
-        touchInput.Enable();
-    }
-    private void OnDisable()
-    {
-        touchInput.Disable();
-    }
-    private void Start()
-    {
-        touchInput.Touch.TouchPoint.performed += ctx => TouchingPerfoming(ctx);
-        touchInput.Touch.TouchPress.canceled += ctx => TouchingCanceled(ctx);
-    }
-    private void TouchingPerfoming(InputAction.CallbackContext ctx)
-    {
-        OnTouchPerfoming?.Invoke(Touchscreen.current.primaryTouch);
-    }
-    private void TouchingCanceled(InputAction.CallbackContext ctx)
-    {
-        OnTouchCanceled?.Invoke(Touchscreen.current.primaryTouch);
-    }
+	private void OnEnable()
+	{
+		if (touchInput == null)
+			touchInput = new TouchInput();
+		touchInput.Touch.TouchPoint.performed += ctx => TouchingPerfoming(ctx);
+		touchInput.Touch.TouchPress.canceled += ctx => TouchingCanceled(ctx);
+		touchInput.Enable();
+	}
+	private void OnDisable()
+	{
+		touchInput.Touch.TouchPoint.performed -= ctx => TouchingPerfoming(ctx);
+		touchInput.Touch.TouchPress.canceled -= ctx => TouchingCanceled(ctx);
+		touchInput.Disable();
+	}
+	private void TouchingPerfoming(InputAction.CallbackContext ctx)
+	{
+		OnTouchPerfoming?.Invoke(Touchscreen.current.primaryTouch);
+	}
+	private void TouchingCanceled(InputAction.CallbackContext ctx)
+	{
+		OnTouchCanceled?.Invoke(Touchscreen.current.primaryTouch);
+	}
 }
